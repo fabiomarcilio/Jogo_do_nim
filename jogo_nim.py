3f49e78fafe33f45f3a1bc1277f6fc7f99cee394
Fabio Marcilio <fabiomarcilio@msn.com>
2020-07-31 14:13:19 -0300

    Correções de erros e aprimoramento da jogada vencedora.

--- jogo_nim.py
+++ jogo_nim.py
@@ -1,21 +1,18 @@
 #Programa com lógica do jogo NIM com vitória da CPU em todos os casos.
 
-print('Bem-vindo ao jogo do NIM! Escolha:')
-print()
-print('1 - para jogar uma partida isolada')
-escolha = int(input('2 - para jogar um campeonato:'))
-
 def computador_escolhe_jogada(n,m):
-    result = n % m
-    if(result == 0):
-        return m
+    if n <= m:
+        return n
     else:
-        return m - 1
+        jogada = n % (m+1)
+        if jogada > 0:
+            return jogada
+        return m
 
 def usuario_escolhe_jogada(n,m):
     print()
     jogada = int(input('Quantas peças você vai tirar?'))
-    while (jogada > m):
+    while(jogada > m or jogada > n or jogada < 1):
         print()
         print('Oops! Jogada inválida! Tente de novo.')
         print()
@@ -47,7 +44,7 @@ def partida():
         print('Agora restam',n,'peças no tabuleiro')
         jogador = 2
     jogada = jogador % 2
-    while(n != 0):
+    while(n > 0):
         if(jogada == 0):
             escolha = usuario_escolhe_jogada(n,m)
             print()
@@ -62,7 +59,7 @@ def partida():
             print('O computador tirou',escolha,'peças')
             print('Agora restam',n,'peças no tabuleiro')
             jogada = jogada - 1
-    if(jogada == 0):
+    if(jogada <= 0):
         print()
         print('O computador ganhou!')
         return 1
@@ -70,6 +67,11 @@ def partida():
         print()
         print('Você ganhou!')
         return 2
+
+print('Bem-vindo ao jogo do NIM! Escolha:')
+print()
+print('1 - para jogar uma partida isolada')
+escolha = int(input('2 - para jogar um campeonato:'))
 
 def campeonato():
     jogo = 1
@@ -83,6 +85,8 @@ def campeonato():
         else:
             placarUser = placarUser + 1
         jogo = jogo + 1
+    print('**** Final do campeonato! ****')
+    print()
     print('Placar: Você',placarUser,'X',placarCpu,' Computador')
 
 if (escolha == 1):
@@ -93,6 +97,7 @@ else:
     print()
     print('Voce escolheu um campeonato!')
     campeonato()
+
 
 
 
